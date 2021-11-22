c0c1d2508d27d5492c206dc3437e6a1876970b02
Cristiano Sobral <cssobral2013@gmail.com>
2021-11-22 15:24:20 -0300

    Update core to latest iosevka-dev

--- font-src/gen/finalize/index.js
+++ font-src/gen/finalize/index.js
@@ -31,11 +31,15 @@ function assignSubRank(glyphStore) {
 //  (AW > 0) have the same width. We use this method to validate whether our "Fixed" subfamilies
 //  are properly built.
 function validateMonospace(para, glyphStore) {
-	if (!para.forceMonospace) return;
 	let awSet = new Set();
 	for (const [u, g] of glyphStore.encodedEntries()) {
 		const aw = Math.round(g.advanceWidth || 0);
 		if (aw > 0) awSet.add(aw);
 	}
-	if (awSet.size > 1) throw new Error("Unreachable! Fixed variant has wide characters");
+	if (para.forceMonospace && awSet.size > 1) {
+		throw new Error("Unreachable! Fixed variant has wide characters");
+	}
+	if (!para.isQuasiProportional && !para.compLig && awSet.size > 2) {
+		throw new Error("Unreachable! Building monospace with more than 2 character widths");
+	}
 }
