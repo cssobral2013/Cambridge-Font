99461659141de9864baa45b0de1883a5bd502add
Cristiano Sobral <cssobral2013@gmail.com>
2023-05-30 15:34:29 -0300

    Restore subset scripts

---
+++ subset-sans.cmd
@@ -0,0 +1,21 @@
+@echo off
+cd ./dist/cambridge-sans/ttf/
+pyftsubset --drop-tables= --unicodes=0-52f,e3f,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-thin.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,e3f,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-extralight.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,e3f,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-light.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,e3f,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-regular.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,e3f,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-medium.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,e3f,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-semibold.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,e3f,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-bold.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,e3f,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-extrabold.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,e3f,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-heavy.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,e3f,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-thinitalic.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,e3f,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-extralightitalic.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,e3f,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-lightitalic.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,e3f,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-italic.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,e3f,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-mediumitalic.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,e3f,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-semibolditalic.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,e3f,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-bolditalic.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,e3f,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-extrabolditalic.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,e3f,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-heavyitalic.ttf
+exit
