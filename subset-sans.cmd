1bed25637b664cc8234e2e7b13df9da6b0b64f4c
Cristiano Sobral <cssobral2013@gmail.com>
2025-01-25 00:30:40 -0300

    Update

---
+++ subset-sans.cmd
@@ -0,0 +1,21 @@
+@echo off
+cd ./dist/cambridge-sans/ttf/
+pyftsubset --drop-tables= --unicodes=0-52f,530-58f,e3f,10fb-1dff,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-normalThinupright.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,530-58f,e3f,10fb-1dff,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-normalExtraLightupright.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,530-58f,e3f,10fb-1dff,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-normalLightupright.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,530-58f,e3f,10fb-1dff,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-normalupright.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,530-58f,e3f,10fb-1dff,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-normalMediumupright.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,530-58f,e3f,10fb-1dff,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-normalSemiBoldupright.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,530-58f,e3f,10fb-1dff,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-normalBoldupright.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,530-58f,e3f,10fb-1dff,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-normalExtraBoldupright.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,530-58f,e3f,10fb-1dff,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-normalHeavyupright.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,530-58f,e3f,10fb-1dff,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-normalThinitalic.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,530-58f,e3f,10fb-1dff,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-normalExtraLightitalic.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,530-58f,e3f,10fb-1dff,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-normalLightitalic.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,530-58f,e3f,10fb-1dff,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-normalitalic.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,530-58f,e3f,10fb-1dff,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-normalMediumitalic.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,530-58f,e3f,10fb-1dff,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-normalSemiBolditalic.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,530-58f,e3f,10fb-1dff,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-normalBolditalic.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,530-58f,e3f,10fb-1dff,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-normalExtraBolditalic.ttf
+pyftsubset --drop-tables= --unicodes=0-52f,530-58f,e3f,10fb-1dff,1e00-200b,2010-2139,213b-215e,2189,a726-a7bf,ab30-fb04 cambridge-sans-normalHeavyitalic.ttf
+exit
